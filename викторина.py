706b142c4cfc05ad9b3f3deffe90c5942769fa83
angelina132rais <114457322+angelina132rais@users.noreply.github.com>
2022-12-20 05:10:22 +0300

    Update викторина.py

--- викторина.py
+++ викторина.py
@@ -2,26 +2,26 @@ from colorama import Fore, Back, Style
 from colorama import init
 init()
  
-print(Style.BRIGHT) # цвет в терминале
+print(Style.BRIGHT) 
 ls = [
     ("Солнце желтое?", "да"),
     ("Летом холодно?", "нет"),
     ("Ты выспался?", "нет"),
     ("Еще вопросы?", "нет")
 ]
-print(Back.RESET) # цвет в терминале
-answers_counter = [0,0] # счетчик ответов
-print(Fore.GREEN) # цвет в терминале RED - красный, YELLOW - желтый и т.д.
+print(Back.RESET) 
+answers_counter = [0,0] 
+print(Fore.GREEN) 
 for q, a in ls:
-    print(q, '[да/нет]' + Style.BRIGHT) # цвет в терминале
+    print(q, '[да/нет]' + Style.BRIGHT)
     answer = input().strip().lower()
     if answer == a:
         print("правильный ответ")
-        answers_counter[0] += 1 # счетчик
-        answers_counter[1] += 1 # счетчик
+        answers_counter[0] += 1 
+        answers_counter[1] += 1 
     else:
         print("неправильный ответ")
-        answers_counter[0] += 1 # счетчик
-print(Style.RESET_ALL) # цвет в терминале
+        answers_counter[0] += 1 
+print(Style.RESET_ALL)
 print("\nДано ответов: {}, Верных ответов: {}".format(answers_counter[0], answers_counter[1]))
 input()
