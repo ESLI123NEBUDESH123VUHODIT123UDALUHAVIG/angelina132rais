efd53257e45787ff972dacdd91ccfa7cc26ee689
angelina132rais <114457322+angelina132rais@users.noreply.github.com>
2022-12-22 12:00:11 +0700

    Create команды.py

---
+++ команды.py
@@ -0,0 +1,22 @@
+s=['0','1','1']
+for x in s:
+    for y in s:
+        for w in s:
+            print ( x+y+w)
+
+s="12345671"
+sp=list(s)
+print(sp)
+sp.count('1')
+print(sp.count('1'))
+a=list(map(int,sp))
+print(a)
+a[-1]
+a[::1]
+
+a.sort(revers=true)
+print(a)
+
+sp=[x for x in range(2)]
+print(sp)
+
