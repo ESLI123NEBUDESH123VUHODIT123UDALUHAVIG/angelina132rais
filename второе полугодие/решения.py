bb74372eab7bb5c1ef12543b199e6b8087e6ef44
angelina132rais <114457322+angelina132rais@users.noreply.github.com>
2023-04-26 10:08:17 +0700

    Update решения.py

--- второе полугодие/решения.py
+++ второе полугодие/решения.py
@@ -235,4 +235,25 @@ with open ('27_B.txt') as f: #открываем файл
         costs.append(cost)
         print(i,sp[i][0],cost)
 #Ответ 5634689219329
-
+#олимпиадная какая-то
+from itertools import product		
+lox=[0,712,673,1075,875,1622,423]		
+ggh=[712,0,1385,1800,1577,2348,1128]		
+opd=[673,1385,0,1499,239,2046,244]		
+mer=[1075,1800,1499,0,1287,551,1266]		
+tin=[875,1577,239,1287,0,1835,442]		
+pav=[1622,2348,2046,551,1835,0,1813]		
+kaa=[423,1128,244,1266,442,1813,0]
+tabl=[lox,ggh,opd,mer,tin,pav,kaa]		
+alf='0123456'		
+a=product(alf,repeat=7)		
+maxi=0		
+for i in a:
+    if all(i.count(x)==1 for x in alf):
+        s=0
+        for l in range(len(i)-1):
+            s+=tabl[int(i[l])][int(i[l+1])]
+            if s >=maxi:
+                maxi=s
+                
+print(maxi)	
